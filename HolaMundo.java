242fe0a4482d1d4c1cf39673930b4cb039c9209e
Javier Vargas <javier.vargas@on24.com>
2022-11-23 18:07:07 -0500

    Ajustado el mensaje secundario para usar el correcto

--- HolaMundo.java
+++ HolaMundo.java
@@ -7,6 +7,6 @@ public class HolaMundo {
 	public static void main(String[] args){
 		HolaMundo hm = new HolaMundo();
 		hm.saludar();
-		System.out.println("Este es mi primera clase en mi nuevo repositorio GIT");
+		System.out.println("Este es el mensaje que ajustamos en hot-fix.");
 	}
 }
