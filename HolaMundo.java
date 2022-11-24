eb24a8482b0582b3cab2f65a9d64a73cbd68c13b
Javier Vargas <javier.vargas@on24.com>
2022-11-23 19:23:07 -0500

    Ampliada la clase HolaMundo con un nuevo mensaje

--- HolaMundo.java
+++ HolaMundo.java
@@ -9,6 +9,7 @@ public class HolaMundo {
 		hm.saludar();
 
 		System.out.println("Esta es una linea modificada desde GitHub!");
+		System.out.println("Esta es una nueva impresion desde nuestro repo local.");
 
 	}
 }
